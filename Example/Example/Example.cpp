53b8e4a2b0e6a90c95779b4a1d83f2b94f7d254d
kluciooo <dominikzucho@gmail.com>
2024-10-11 17:20:16 +0200

    Nowe funkcje (usuwanie z listy)
    usun_z_konca() usun_z_poczatku() usun_z_indexu()

--- Example/Example/Example.cpp
+++ Example/Example/Example.cpp
@@ -22,25 +22,31 @@ public:
 	**/
 
 	void wyswietl_od_poczatku() {
-		Wezel* wskaznik = poczatek;
-		int i = 0;
-		do {
-			if (wskaznik->nastepny != nullptr) cout << "(index: " << i << "; wartosc: " << wskaznik->dane << "), ";
-			else cout << "(index: " << i << "; wartosc: " << wskaznik->dane << ") ";
-			wskaznik = wskaznik->nastepny;
-			i++;
-		} while (wskaznik != nullptr);
+		if (!koniec) {}
+		else {
+			Wezel* wskaznik = poczatek;
+			int i = 0;
+			do {
+				if (wskaznik->nastepny != nullptr) cout << "(index: " << i << "; wartosc: " << wskaznik->dane << "), ";
+				else cout << "(index: " << i << "; wartosc: " << wskaznik->dane << ") ";
+				wskaznik = wskaznik->nastepny;
+				i++;
+			} while (wskaznik != nullptr);
+		}
 	}
 
 	void wyswietl_od_konca() {
-		Wezel* wskaznik = koniec;
-		int i = ilosc - 1;
-		do {
-			if (wskaznik->poprzedni != nullptr) cout << "(index: " << i << "; wartosc: " << wskaznik->dane << "), ";
-			else cout << "(index: " << i << "; wartosc: " << wskaznik->dane << ") ";
-			wskaznik = wskaznik->poprzedni;
-			i--;
-		} while (wskaznik != nullptr);
+		if (!koniec) {}
+		else {
+			Wezel* wskaznik = koniec;
+			int i = ilosc - 1;
+			do {
+				if (wskaznik->poprzedni != nullptr) cout << "(index: " << i << "; wartosc: " << wskaznik->dane << "), ";
+				else cout << "(index: " << i << "; wartosc: " << wskaznik->dane << ") ";
+				wskaznik = wskaznik->poprzedni;
+				i--;
+			} while (wskaznik != nullptr);
+		}
 	}
 
 	/*
@@ -81,21 +87,78 @@ public:
 		}
 		ilosc++;
 	}
+
+	/*
+	@Brief walidacja dodana!!
+	*/
 	void dodaj_na_index(int wartosc, int index) {
-		Wezel* nowyWezel = new Wezel;
-		Wezel* wskaznik = poczatek;
-		for (int i = 0; i < index - 1; i++)
-		{
-			wskaznik = wskaznik->nastepny;
+		if (index > 0 && index < ilosc) {
+			Wezel* nowyWezel = new Wezel;
+			Wezel* wskaznik = poczatek;
+			for (int i = 0; i < index - 1; i++)
+			{
+				wskaznik = wskaznik->nastepny;
+			}
+			nowyWezel->nastepny = wskaznik->nastepny;
+			wskaznik->nastepny->poprzedni = nowyWezel;
+			wskaznik->nastepny = nowyWezel;
+			nowyWezel->poprzedni = wskaznik;
+			nowyWezel->dane = wartosc;
+			ilosc++;
 		}
-		nowyWezel->nastepny = wskaznik->nastepny;
-		wskaznik->nastepny->poprzedni = nowyWezel;
-		wskaznik->nastepny = nowyWezel;
-		nowyWezel->poprzedni = wskaznik;
-		nowyWezel->dane = wartosc;
-		ilosc++;
+		else if (index == 0) dodaj_na_poczatek(wartosc);
+		else if (index == ilosc) dodaj_na_koniec(wartosc);
+		else cout << "Index poza listą";
 	}
 
+	/*
+	
+	Usuwanie elementów
+
+	*/
+
+	void usun_z_poczatku() {
+		if (!poczatek) {
+			cout << "brak elementow\n";
+		}
+		else
+		{
+			Wezel* chwilowy = poczatek;
+			poczatek = poczatek->nastepny;
+			poczatek->poprzedni = nullptr;
+			delete chwilowy;
+		}
+		ilosc--;
+	}
+	void usun_z_konca() {
+		if (!koniec) {
+			cout << "brak elementow\n";
+		}
+		else
+		{
+			Wezel* chwilowy = koniec;
+			koniec = koniec->poprzedni;
+			koniec->nastepny = nullptr;
+			delete chwilowy;
+		}
+		ilosc--;
+	}
+	void usun_z_indexu(int index) {
+		if (index > 0 && index < ilosc) {
+			Wezel* wskaznik = poczatek;
+			for (int i = 0; i != index; i++)
+			{
+				wskaznik = wskaznik->nastepny;
+			}
+			Wezel* chwilowy = wskaznik;
+			wskaznik->poprzedni->nastepny = wskaznik->nastepny;
+			wskaznik->nastepny->poprzedni = wskaznik->poprzedni;
+			delete chwilowy;
+		}
+		else {
+			cout << "Poza lista!";
+		}
+	}
 };
 
 int main()
@@ -104,11 +167,17 @@ int main()
 	lista.dodaj_na_poczatek(1);
 	lista.dodaj_na_poczatek(2);
 	lista.dodaj_na_poczatek(3);
-	lista.dodaj_na_koniec(0);
-	
-	lista.wyswietl_od_poczatku();
 	lista.dodaj_na_index(99, 2);
+
+
+
+	lista.wyswietl_od_poczatku();
+	cout << "\n==========\n";
+	
+	lista.dodaj_na_index(9,4);
+
+
 	cout << "\n==========\n";
 	lista.wyswietl_od_poczatku();
-	lista.wyswietl_od_konca();
+	//lista.wyswietl_od_konca();
 }
