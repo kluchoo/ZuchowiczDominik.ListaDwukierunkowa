de064a664a0eb06eb91c00f692d4cf262939203e
kluciooo <dominikzucho@gmail.com>
2024-10-11 15:15:19 +0200

    Dodanie podstawowych funkcji
    funkcje: wyswietl_od_poczatku, dodaj_na_poczatek

--- Example/Example/Example.cpp
+++ Example/Example/Example.cpp
@@ -1,7 +1,48 @@
 ﻿#include <iostream>
 using namespace std;
 
+struct Wezel {
+	int dane = 0;
+	Wezel* nastepny = nullptr;
+	Wezel* poprzedni = nullptr;
+};
+
+class ListaDwukierunkowa {
+private:
+	Wezel* poczatek;
+	Wezel* koniec;
+public:
+	ListaDwukierunkowa() : poczatek(nullptr), koniec(nullptr) {}
+
+	void wyswietl_od_poczatku() {
+		Wezel* wskaznik = poczatek;
+		do {
+			cout << wskaznik->dane << ", ";
+			wskaznik = wskaznik->nastepny;
+		} while (wskaznik != nullptr);
+	}
+
+	void dodaj_na_poczatek(int wartosc) {
+		Wezel* nowyWezel = new Wezel;
+		if (!poczatek) {
+			poczatek = nowyWezel;
+			koniec = nowyWezel;
+			nowyWezel->dane = wartosc;
+		}
+		else
+		{
+			nowyWezel->nastepny = poczatek;
+			poczatek->poprzedni = nowyWezel;
+			poczatek = nowyWezel;
+			nowyWezel->dane = wartosc;
+		}
+	}
+};
+
 int main()
 {
-    int a = 5;
+	ListaDwukierunkowa lista;
+	lista.dodaj_na_poczatek(1);
+	lista.dodaj_na_poczatek(2);
+	lista.wyswietl_od_poczatku();
 }
